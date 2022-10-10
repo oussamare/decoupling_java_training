c9add2891cef874931da246ae16eb28c6217ad58
rechah oussaama <rechah@et.esiea.fr>
2022-10-10 17:31:08 +0200

    done

--- src/main/java/fr/lernejo/guessgame/Launcher.java
+++ src/main/java/fr/lernejo/guessgame/Launcher.java
@@ -5,31 +5,24 @@ import java.security.SecureRandom;
 import fr.lernejo.logger.Logger;
 import fr.lernejo.logger.LoggerFactory;
 import java.security.SecureRandom;
+import java.util.regex.Pattern;
 
 public class Launcher {
     private static final Logger logger = LoggerFactory.getLogger("launcher");
 
     public static void main(String[] args) {
-        Simulation simulation;
-
-        if (args.length == 1 && args[0].equals("-interactive")) {
-            HumanPlayer humanPlayer = new HumanPlayer();
-            simulation = new Simulation(humanPlayer);
-            SecureRandom random = new SecureRandom();
-            long randomNumber = random.nextInt(100);
-            simulation.initialize(randomNumber);
-            simulation.loopUntilPlayerSucceed(Long.MAX_VALUE);
-        }
-        else if (args.length == 2 && args[0].equals("-auto") && args[1].matches("\\d+")) {
-            ComputerPlayer computerPlayer = new ComputerPlayer();
-            simulation = new Simulation(computerPlayer);
-            long number = Long.parseLong(args[1]);
-            simulation.initialize(number);
-            simulation.loopUntilPlayerSucceed(1000);
-        }
-        else {
-            logger.log("Vous avez deux chois: si vous voulez jouer avec un humain vous devez choisir -interactive ");
-            logger.log("Ou si vous voulez jouer avec un ordinateur vous devez choisir -auto et le deuxième paramétre doit être un numéro ");
+        Simulation sim = null;
+        if (args[0].equals("-interactive")) {
+            sim = new Simulation(new HumanPlayer());
+            sim.initialize(new SecureRandom().nextInt(100));
+            sim.loopUntilPlayerSucceed(Long.MAX_VALUE);
+        } else if (args[0].equals("-auto") && Pattern.compile("-?\\d+(\\.\\d+)?").matcher(args[1]).matches()) {
+            sim = new Simulation(new ComputerPlayer(Long.parseLong(args[1])));
+            sim.initialize(new SecureRandom().nextLong(Long.parseLong(args[1])));
+            sim.loopUntilPlayerSucceed(1000);
+        } else {
+            System.out.println("Pour lancer le mode interactive, taper -interactive.\n" +
+                "Pour lancer le mode auto, taper -auto <max_value>");
         }
     }
 }
--- src/main/java/fr/lernejo/guessgame/Simulation.java
+++ src/main/java/fr/lernejo/guessgame/Simulation.java
@@ -3,68 +3,65 @@ package fr.lernejo.guessgame;
 import fr.lernejo.logger.Logger;
 import fr.lernejo.logger.LoggerFactory;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Simulation {
 
 
-        private final Logger logger = LoggerFactory.getLogger("simulation");
-        private final Player player;  //TODO add variable type
-        private long numberToGuess; //TODO add variable type
+    private final Logger logger = LoggerFactory.getLogger(Simulation.class.getName());
+    private Player player;  //TODO add variable type
+    private long numberToGuess; //TODO add variable type
 
-        public Simulation(Player player) {
-            this.player=player;
+    public Simulation(Player player) {
+        this.player = player;
+    }
+
+    public void initialize(long numberToGuess) {
+        this.numberToGuess = numberToGuess;
+    }
+
+    /**
+     * @return true if the player have guessed the right number
+     */
+    private boolean nextRound() {
+        long value = player.askNextGuess();
+        if(value > numberToGuess){
+            player.respond(false);
+        }else if (value < numberToGuess){
+            player.respond(true);
+        }else{
+            logger.log("The player found the number");
+            return true;
         }
+        return false;
+    }
 
-        public void initialize(long numberToGuess) {
-            //TODO implement me
-            this.numberToGuess=numberToGuess;
+    public void loopUntilPlayerSucceed() {
+        while (!nextRound()){
         }
-
-        /**
-         * @return true if the player have guessed the right number
-         */
-        private boolean nextRound() {
-            //TODO implement me
-            long playernumber = player.askNextGuess();
-            if (playernumber==numberToGuess){
-                logger.log("bien");
-                return true;
+    }
+    public void loopUntilPlayerSucceed(long count) {
+        int cmp = 0;
+        long start = System.currentTimeMillis();
+        do{
+            if(nextRound()){
+                break;
             }
-            else {
-                player.respond(playernumber>numberToGuess);
-                return false;
-            }
+        }while(cmp++<=count);
+        long end = System.currentTimeMillis();
+        Date elapsed = new Date(end-start);
+        DateFormat formatter = new SimpleDateFormat("mm:ss.SSS");
 
-
+        if(cmp >= count){
+            logger.log("The user has found the number guess in "+cmp+" loop and "+formatter.format(elapsed));
+            System.out.println("You have found the number in "+formatter.format(elapsed));
+        }else{
+            logger.log("The user has not found the number guess in "+cmp+" loop and "+formatter.format(elapsed));
+            System.out.println("You have not found the number in "+formatter.format(elapsed));
         }
-
-        public void loopUntilPlayerSucceed(long maxValue) {
-
-            boolean  toutou;
-            //TODO implement me
-            int i = 0;
-            long start=System.currentTimeMillis();
-            do {
-              toutou= this.nextRound();
-
-                if (i>=maxValue)
-                    break;
-                i++;
-            } while (!toutou);
-            long end = System.currentTimeMillis();
-
-            long gameTime = end - start;
-            long minutes = gameTime / 60000;
-            gameTime %= 60000;
-            long seconds = gameTime / 1000;
-            gameTime %= 1000;
-            long milliseconds = gameTime;
-            logger.log(String.format("%02d:%02d.%d", minutes, seconds, milliseconds));
-            if(toutou)
-                logger.log("joueur gagne");
-            else
-                logger.log("joueur perdu");
-
-        }
+    }
 
     }
 
